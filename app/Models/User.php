71cf5c1b6184dcd83c6df1701ca07526891ab879
iztileuoff <bektemir0714@gmail.com>
2022-12-13 13:03:32 +0500

    with media-library

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements HasMedia
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'phone', 'password',
     ];
 
     /**
